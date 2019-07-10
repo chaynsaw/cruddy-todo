89ebf58aaad86f121b542490ed133dded293ed76
Chaynor Hsiao <chaynsaw@gmail.com>
2019-07-09 21:40:07 -0700

    Implement testing files

---
+++ testFiles.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+// fs.writeFileSync('blah.txt', 'blah');
+// fs.readFile('blah.txt', 'utf8', (err, data) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(data)
+//   }
+// })
+// console.log(fs.readFileSync('blah.txt', 'utf8'));
+
+fs.openSync('blah.txt', (err, id) => {
+  if(err) 
+};
+console.log('WORD')
